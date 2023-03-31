2e4ec32a13e89e6b1eaec2fef2f89714b6ab7e9e
RohDamin <57509627+RohDamin@users.noreply.github.com>
2023-03-31 17:11:23 +0900

    git test

---
+++ app/src/main/java/com/example/code221020_bookexample/BookVO.java
@@ -0,0 +1,5 @@
+package com.example.code221020_bookexample;
+
+public class BookVO {
+    test git;
+}
---
+++ app/src/main/java/com/example/code221020_bookexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.code221020_bookexample;
+//참고: https://wltsp222-tistroy-com.tistory.com/31
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
